93c8b4b58ef4438c98a0062b594ebc09cf5251e3
James Dindi <dindijjames@gmail.com>
2018-12-11 17:57:38 +0300

    Working with event handlers between different components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,64 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {
+        name: "Daktari",
+        age: 23
+      },
+      {
+        name: "Daisy",
+        age: 21
+      },
+      {
+        name: "Dede",
+        age: 20
+      }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler  = (newName, newAge) => {
+    this.setState({
+      persons: [
+        {
+          name: newName,
+          age: 23
+        },
+        {
+          name: "Khaindi",
+          age: newAge
+        },
+        {
+          name: "Dede",
+          age: 20
+        }
+      ]
+    })
+
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name={'James'} age={23} />
-        <Person name={'Daisy'} age={21}>My hobbies: eating chocolate</Person>
-        <Person name={'Dede'} age={20} />
+        <button onClick={this.switchNameHandler.bind(this, 'Johannesburg', 22)}>Switch Name</button>
+        <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+        />
+        <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Sweetie', 19)}>My hobbies: eating chocolate
+        </Person>
+        <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+        />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
